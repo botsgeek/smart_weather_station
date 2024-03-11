9d17cb55e2022d891df2da2ba068babc7be3b8f6
RSaliu <ridwanriley@gmail.com>
2024-03-11 22:41:29 +0100

    [ticket-esp32-wifi] - add documentation to project

--- Drivers/ESPWIFI/Inc/esp32_wifi.h
+++ Drivers/ESPWIFI/Inc/esp32_wifi.h
@@ -1,3 +1,16 @@
+/**
+ * @file esp32_wifi.h
+ * @author Rilwan (rsaliu@botsgeeks.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-11
+ *
+ * @copyright Copyright (c) 2024
+ *
+ *
+ * This library enables the stm32 board to connect to WiFi using AT commands
+ * with as esp32 over UART
+ */
 #ifndef __esp32_WIFI_H___
 #define __esp32_WIFI_H___
 #include <stdint.h>
@@ -10,13 +23,63 @@
 extern "C"
 {
 #endif
+
+    /**
+     * @brief esp32_wifi_t object shadow
+     *
+     */
     typedef struct esp32_wifi_t esp32_wifi_t;
+    /**
+     * @brief Creates wifi object
+     *
+     * @param esp32_chat_object
+     * @return esp32_wifi_t*
+     */
     esp32_wifi_t *esp32WifiCreate(esp32_chat_t *esp32_chat_object);
+    /**
+     * @brief Initializes WiFi
+     *
+     * @param esp32_wifi_object
+     * @return error_type_t
+     */
     error_type_t esp32WifiInit(esp32_wifi_t *esp32_wifi_object);
+    /**
+     * @brief Connects to Wifi
+     *
+     * @param esp32_wifi_object
+     * @param SSID
+     * @param password
+     * @return error_type_t
+     */
     error_type_t esp32WifiConnect(esp32_wifi_t *esp32_wifi_object, char *SSID, char *password);
+    /**
+     * @brief Disconnects WiFi
+     *
+     * @param esp32_wifi_object
+     * @return error_type_t
+     */
     error_type_t esp32WifiDisconnect(esp32_wifi_t *esp32_wifi_object);
+    /**
+     * @brief Check if wifi is connected
+     *
+     * @param esp32_wifi_object
+     * @param status
+     * @return error_type_t
+     */
     error_type_t esp32WifiIsconnected(esp32_wifi_t *esp32_wifi_object, bool *status);
+    /**
+     * @brief Deinitializes wifi object
+     *
+     * @param esp32_wifi_object
+     * @return error_type_t
+     */
     error_type_t esp32WifiDeInit(esp32_wifi_t *esp32_wifi_object);
+    /**
+     * @brief destroys wifi object
+     *
+     * @param esp32_wifi_object
+     * @return error_type_t
+     */
     error_type_t esp32WifiDestroy(esp32_wifi_t **esp32_wifi_object);
 #ifdef __cplusplus
 }
