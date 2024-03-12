ac60240ad8510fbb415fcbcad77aaeb6f09690b3
Adeboye Oyeniyi <oyeniyiadeboye@gmail.com>
2024-03-12 21:10:34 +0100

    initialising the DS3231 I2c library

--- Drivers/DS3231/Inc/DS3231.h
+++ Drivers/DS3231/Inc/DS3231.h
@@ -1,7 +1,19 @@
 #ifndef __DS3231_H___
 #define __DS3231_H___
 
+I2C_HandleTypeDef hi2c_DS3231;
+
+	typedef struct
+	{
 
 
+	}ds3231_config_t;
+
+	typedef struct
+	{
+
+	}ds3231_time_t;
+
+	 error_type_t ds3231Init(ds3231_t *ds3231_object);
 
 #endif
--- Drivers/DS3231/Src/DS3231.c
+++ Drivers/DS3231/Src/DS3231.c
@@ -1,1 +1,3 @@
 #include <DS3231.h>
+#include <stdbool.h>
+#include <common_headers.h>
