7e57907575c3a1404999304de8bba8bcffc30686
Dave-Olo <destineddave@gmail.com>
2024-03-14 12:46:33 +0100

    [ticket-SDcard-wrapper-driver] sd_card_files

--- Drivers/SDCard/Inc/sdcard.h
+++ Drivers/SDCard/Inc/sdcard.h
@@ -1,33 +1,52 @@
-#ifndef __SDCARD_H___
-#define __SDCARD_H___
+#ifndef __SDCARD_H__
+#define __SDCARD_H__
+
 #include <stdint.h>
+#include <stdbool.h>
 #include <common_headers.h>
 #include <stm32f411xe.h>
 #include "stm32f4xx_hal.h"
+#include "ff.h"
+#include "fatfs.h"
+
+
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-    typedef struct sdcard_t sdcard_t;
-    typedef struct
-    {
-        GPIO_TypeDef *gpio_port;
-        uint16_t gpio_pin;
-    } sdcard_config_t;
+typedef struct sdcard_t sdcard_t;
+typedef struct {
+    SPI_HandleTypeDef *spi_handle;
+    GPIO_TypeDef *cs_port;
+    uint16_t cs_pin;
+    //file object pointer FIL fil
+} sdcard_config_t;
 
-    typedef struct
-    {
-        float humidity;
-        float temperature;
-    } sdcard_data_t;
+typedef struct {
+    float total_space_mb;
+    float free_space_mb;
+} sdcard_info_t;
 
-    sdcard_t *sdcardCreate(const sdcard_config_t *config);
-    error_type_t sdcardInit(sdcard_t *sdcard_object);
-    error_type_t sdcardDeInit(sdcard_t *sdcard_object);
-    error_type_t sdcardRead(sdcard_t *sdcard_object, sdcard_data_t *data);
-    error_type_t sdcardWrite(sdcard_t *sdcard_object, sdcard_data_t *data);
-    error_type_t sdcardDestroy(sdcard_t **sdcard_object);
+typedef struct {
+    uint8_t *buffer;
+    uint32_t size;
+} sdcard_data_t;
+
+//typedef struct sdcard_t sdcard_t;
+
+sdcard_t *sdcard_create(const sdcard_config_t *config);
+error_type_t sdcard_Init(sdcard_t *sdcard_object);
+// error_type_t sdcard_get_info(const FATFS *fs, sdcard_info_t *info);
+error_type_t sdcard_get_info(FATFS *fs, sdcard_info_t *info);
+error_type_t sdcard_create_file(sdcard_t *sdcard_object, const char *filename, FIL* file);
+error_type_t sdcard_read_file(sdcard_t *sdcard_object, const sdcard_data_t *data, FIL* file);
+error_type_t sdcard_write_file(sdcard_t *sdcard_object, const sdcard_data_t *data, FIL* file);
+error_type_t sdcard_delete_file(sdcard_t *sdcard_object, const char *filename, FIL* file);
+error_type_t sdcard_close_file(sdcard_t *sdcard_object, FIL* file);
+error_type_t sdcard_Deinit(sdcard_t *sdcard_object);
+error_type_t sdcard_destroy(sdcard_t **sdcard_object);
+//add API for file close
 #ifdef __cplusplus
 }
 #endif
-#endif
+
+#endif 
--- Drivers/SDCard/Src/sdcard.c
+++ Drivers/SDCard/Src/sdcard.c
@@ -1,43 +1,77 @@
-#include "sdcard.h"
+#include <sdcard.h>
+#include <string.h>
 #include <stdbool.h>
 #include <common_headers.h>
+#include "ff.h"
+sdcard_t *sdcard_object;
 
-
-struct sdcard_t
-{
-    GPIO_TypeDef *gpio_port;
-    uint16_t gpio_pin;
+struct sdcard_t {
+    SPI_HandleTypeDef *spi_handle;
+    GPIO_TypeDef *cs_port;
+    uint16_t cs_pin;
     bool initialized;
 };
 
-error_type_t sdcardInit(sdcard_t *sdcard_object)
-{
+
+    sdcard_t *sdcard_create(const sdcard_config_t *config) {
+  
+    if (config == NULL || config->spi_handle == NULL)
+    {
+        return NULL;
+    }
+
+   sdcard_t *sdcard_object = (sdcard_t *)malloc(sizeof(sdcard_t));
+   if (sdcard_object == NULL) {
+        return NULL;
+    }
+
+   sdcard_object->spi_handle = config->spi_handle;
+   sdcard_object->cs_pin = config->cs_pin;
+   sdcard_object->cs_port = config->cs_port;
+   sdcard_object->initialized = false;
+
+   return sdcard_object;
+}
+
+// Function to initialize the SD card
+error_type_t sdcard_Init(sdcard_t *sdcard_object) {
+//     FATFS FatFs; 	
+//     ///FIL fil; 		
+//     FRESULT fres; 
+
+//   // Open the file system
+//   fres = f_mount(&FatFs, "", 1); //1=mount now
+//   if (fres != FR_OK) {
+// 	printf("f_mount error (%i)\r\n", fres);
+// 	while(1);
+//   }
+    // Implement the initialization process
     if (!sdcard_object)
     {
         return SYSTEM_NULL_PARAMETER;
     }
 
-    if (sdcard_object->gpio_port == GPIOA)
+    if (sdcard_object->cs_port == GPIOA)
     {
         __HAL_RCC_GPIOA_CLK_ENABLE();
     }
-    else if (sdcard_object->gpio_port == GPIOB)
+    else if (sdcard_object->cs_port == GPIOB)
     {
         __HAL_RCC_GPIOB_CLK_ENABLE();
     }
-    else if (sdcard_object->gpio_port == GPIOC)
+    else if (sdcard_object->cs_port == GPIOC)
     {
         __HAL_RCC_GPIOC_CLK_ENABLE();
     }
-    else if (sdcard_object->gpio_port == GPIOD)
+    else if (sdcard_object->cs_port == GPIOD)
     {
         __HAL_RCC_GPIOD_CLK_ENABLE();
     }
-    else if (sdcard_object->gpio_port == GPIOE)
+    else if (sdcard_object->cs_port == GPIOE)
     {
         __HAL_RCC_GPIOE_CLK_ENABLE();
     }
-    else if (sdcard_object->gpio_port == GPIOH)
+    else if (sdcard_object->cs_port == GPIOH)
     {
         __HAL_RCC_GPIOH_CLK_ENABLE();
     }
@@ -46,104 +80,169 @@ error_type_t sdcardInit(sdcard_t *sdcard_object)
         return SYSTEM_INVALID_PIN_PORT;
     }
 
-    // GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    // GPIO_InitStruct.Pin = _object->gpio_pin;
-    // GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-    // GPIO_InitStruct.Pull = GPIO_PULLUP;
-    // GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
-    // HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    // // set pin high as default
-    // HAL_GPIO_WritePin(dht22_object->gpio_port, dht22_object->gpio_pin, GPIO_PIN_SET);
-    // // set pin high as default
-    // dht22_object->initialized = true;
-    // return SYSTEM_OK;
-}
-
-sdcard_t *sdcardCreate(const sdcard_config_t *config)
-{
-    if (!config)
-    {
-        return NULL;
-    }
-    sdcard_t *sdcard_object = (sdcard_t *)malloc(sizeof(dht22_t));
-    sdcard_object->gpio_pin = config->gpio_pin;
-    sdcard_object->gpio_port = config->gpio_port;
-    return sdcard_object;
-}
-
-error_type_t dht22Read(dht22_t *dht22_object, dht22_data_t *data)
-{
-    error_type_t err;
-    if (dht22_object == NULL || data == NULL)
-    {
-        return SYSTEM_NULL_PARAMETER;
-    }
-    if (!dht22_object->initialized)
-    {
-        return SYSTEM_INVALID_STATE;
-    }
-    trigger(dht22_object);
-    err = detectAck(dht22_object);
-    if (err != SYSTEM_OK)
-    {
-        return err;
-    }
-    uint8_t bytes[BYTES_IN_DATA] = {0};
-    err = getData(dht22_object, bytes);
-    if (err != SYSTEM_OK)
-    {
-        return err;
-    }
-    err = validateCheckSum(bytes);
-    if (err != SYSTEM_OK)
-    {
-        return err;
-    }
-    convert_value(bytes, data);
+    GPIO_InitStruct.Pin = sdcard_object->cs_pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    // set pin high as default
+    HAL_GPIO_WritePin(sdcard_object->cs_port, sdcard_object->cs_pin, GPIO_PIN_SET);
+    // set pin high as default
+    //sdcard_object->initialized = true;
     return SYSTEM_OK;
 }
 
 
-error_type_t dht22Read(dht22_t *dht22_object, dht22_data_t *data)
-{
-    error_type_t err;
-    if (dht22_object == NULL || data == NULL)
-    {
+error_type_t sdcard_get_info(FATFS *fs, sdcard_info_t *info) {
+    DWORD fre_clust, fre_sect, tot_sect;
+    FRESULT res;
+
+    if (!fs || !info) {
         return SYSTEM_NULL_PARAMETER;
     }
-    if (!dht22_object->initialized)
-    {
-        return SYSTEM_INVALID_STATE;
+
+//     // Get free space
+    res = f_getfree("", &fre_clust, &fs);
+    if (res != FR_OK) {
+        return SYSTEM_OPERATION_FAILED;
     }
-    trigger(dht22_object);
-    err = detectAck(dht22_object);
-    if (err != SYSTEM_OK)
-    {
-        return err;
+
+//     // Calculate total sectors and free sectors
+    tot_sect = (fs->n_fatent - 2) * fs->csize;
+    fre_sect = fre_clust * fs->csize;
+
+//     // Populate the info structure
+    info->total_space_mb = ((tot_sect / 2)/1024);
+    info->free_space_mb = ((fre_sect / 2)/1024);
+
+    // info->total_space_mb = ((tot_sect * 512) / 1024) / 1024;
+    // info->free_space_mb = ((fre_sect * 512) / 1024) / 1024;
+
+
+   return SYSTEM_OK;
+}
+
+
+error_type_t sdcard_create_file(sdcard_t *sdcard_object, const char *filename, FIL* file) {
+    // Implement the code to create a file on the SD card
+     if (!sdcard_object || !filename || !file) {
+        return SYSTEM_NULL_PARAMETER;
     }
-    uint8_t bytes[BYTES_IN_DATA] = {0};
-    err = getData(dht22_object, bytes);
-    if (err != SYSTEM_OK)
-    {
-        return err;
+    
+    FRESULT file_res;
+
+    // Open or create a file
+    file_res = f_open(file, filename,  FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS | FA_READ);
+    if (file_res != FR_OK) {
+        // Handle error
+        printf("Failed to open or create file: %s\n", filename);
+        return SYSTEM_OPERATION_FAILED;
     }
-    err = validateCheckSum(bytes);
-    if (err != SYSTEM_OK)
-    {
-        return err;
+
+    // file_res = f_open(file, filename, FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
+    // if (file_res != FR_OK) {
+    //      if (file_res == FR_EXIST) {
+    //         printf("Failed to create file '%s': File already exists\n", filename);
+    //      } else {
+    //     printf("Failed to open or create file '%s'\n", filename);
+    //     }
+    //     return SYSTEM_OPERATION_FAILED;
+    // }
+
+
+    return SYSTEM_OK;
+
+}
+
+// Function to read data from a file on the SD card
+error_type_t sdcard_read_file(sdcard_t *sdcard_object, const sdcard_data_t *data,FIL* file){
+
+    if (!sdcard_object || !file || !data) {
+        return SYSTEM_NULL_PARAMETER;
     }
-    convert_value(bytes, data);
+
+
+    FRESULT fres;
+
+    // Read data from the file
+    UINT bytesRead;
+    fres = f_read(file, data->buffer, data->size, &bytesRead);
+    if (fres != FR_OK) {
+        return SYSTEM_OPERATION_FAILED;
+    }
+
     return SYSTEM_OK;
 }
 
 
-error_type_t dht22Destroy(dht22_t **dht22_object)
-{
-    if (*dht22_object)
+error_type_t sdcard_write_file(sdcard_t *sdcard_object, const sdcard_data_t *data,FIL* file){
+    if (!sdcard_object || !data ||!file ) {
+        return SYSTEM_NULL_PARAMETER;
+     }
+
+    FRESULT fres_write;
+    UINT bytesWritten;
+    
+    fres_write = f_write(file, data->buffer, data->size, &bytesWritten);
+    if (fres_write != FR_OK) {
+        return SYSTEM_OPERATION_FAILED;
+    }
+
+    return SYSTEM_OK;
+ }
+
+error_type_t sdcard_close_file(sdcard_t *sdcard_object, FIL* file) {
+    if (!sdcard_object || !file) {
+        return SYSTEM_NULL_PARAMETER;
+    }
+
+    // Close the file
+    FRESULT fres = f_close(file);
+    if (fres != FR_OK) {
+        return SYSTEM_OPERATION_FAILED;
+    }
+
+    return SYSTEM_OK;
+}
+error_type_t sdcard_delete_file(sdcard_t *sdcard_object, const char *filename, FIL* file) {
+    if (!sdcard_object || !filename || !file) {
+        return SYSTEM_NULL_PARAMETER;
+    }
+
+    // Delete the file
+    FRESULT fres = f_unlink(filename);
+    if (fres != FR_OK) {
+        return SYSTEM_OPERATION_FAILED;
+    }
+
+    return SYSTEM_OK;
+}
+
+
+// //Function to deinitialize the SD card
+error_type_t sdcard_Deinit(sdcard_t *sdcard_object){
+    
+    if (sdcard_object)
     {
-        free(*dht22_object);
-        *dht22_object = NULL;
+        sdcard_object->initialized = false;
+        return SYSTEM_OK;
+    }
+    else
+    {
+        return SYSTEM_NULL_PARAMETER;
+    }
+
+}
+
+// // Function to destroy the sdcard_t structure
+error_type_t sdcard_destroy(sdcard_t **sdcard_object) {
+    // Implement the code to free the memory allocated for the sdcard_t structure
+    if (*sdcard_object)
+    {
+        free(*sdcard_object);
+        *sdcard_object = NULL;
         return SYSTEM_OK;
     }
     else
@@ -152,15 +251,3 @@ error_type_t dht22Destroy(dht22_t **dht22_object)
     }
 }
 
-error_type_t dht22DeInit(dht22_t *dht22_object)
-{
-    if (dht22_object)
-    {
-        dht22_object->initialized = false;
-        return SYSTEM_OK;
-    }
-    else
-    {
-        return SYSTEM_NULL_PARAMETER;
-    }
-}
